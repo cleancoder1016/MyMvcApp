557ea91c5636ce784a2a0e0833a303e63ef672f7
Siva Rama Krishna Prasad Changala <changala.2@wright.edu>
2025-01-03 06:52:24 -0500

    created model for the user

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMvcApp.Models 
+{
+    public class User
+    {
+        public int ID {get; set;}
+        public string Name {get; set;}
+        public string Email {get; set;}
+        public string Password {get; set;}
+        public string PhoneNumber {get; set;}
+        public bool isActive {get; set;}
+    }
+}
